4d7d71eaad86786d678496d5805757fa8c337c86
Finn Ellis <relsqui@chiliahedron.com>
2015-11-18 11:06:18 -0800

    Add a default number of rounds of scheduling.

--- encryption.py
+++ encryption.py
@@ -15,19 +15,23 @@ import time
 # Use OS-provided randomness instead of only software.
 urandom = random.SystemRandom(time.clock())
 
-# This is a default and can be overridden in function calls.
+# These are just defaults, based on the CipherSaber2 specification.
+# They can be overridden in the actual function calls.
 IV_LENGTH = 10
+ROUNDS = 200
 
 
-def keystream(stream_length, rounds, key):
+def keystream(stream_length, key, rounds = None):
     """
     Generate an RC4 keystream of the given length, doing a specified number
     of rounds of key scheduling and encrypting with the key provided.
     """
-    S = range(256)
+    if rounds == None:
+        rounds = ROUNDS
 
     # Do key scheduling.
     j = 0
+    S = range(256)
     while rounds:
         for i in range(256):
             j = (j + S[i] + ord(key[i % len(key)])) % 256
@@ -61,30 +65,34 @@ def random_iv(length = None):
     return iv
 
 
-def encrypt(message, rounds, key, iv = None):
+def encrypt(message, key, rounds = None, iv = None):
     """
     Encrypt a message with the given key, doing the specified number of rounds
     of key scheduling. Creates a random IV if none was provided.
     """
+    if rounds == None:
+        rounds = ROUNDS
     if iv == None:
         iv = random_iv()
-    stream = keystream(len(message), rounds, key+iv)
+    stream = keystream(len(message), key+iv, rounds)
     ciphertext = ""
     for i in range(len(message)):
         ciphertext += chr(ord(message[i]) ^ stream[i])
     return iv + ciphertext
 
 
-def decrypt(ciphertext, rounds, key, iv_length = None):
+def decrypt(ciphertext, key, rounds = None, iv_length = None):
     """
     Decrypt a message with the given key, doing the specified number of rounds
     of key scheduling. If no IV length is provided, assumes the default.
     """
+    if rounds == None:
+        rounds = ROUNDS
     if iv_length == None:
         iv_length = IV_LENGTH
     iv = ciphertext[:iv_length]
     ciphertext = ciphertext[iv_length:]
-    stream = keystream(len(ciphertext), rounds, key+iv)
+    stream = keystream(len(ciphertext), key+iv, rounds)
     plaintext = ""
     for i in range(len(ciphertext)):
         plaintext += chr(ord(ciphertext[i]) ^ stream[i])
@@ -98,30 +106,30 @@ if __name__ == "__main__":
 
     print("-- Testing stream generation. --")
     print("Generating a 10-byte stream, 200 rounds, key 'testkey'")
-    stream = keystream(10, 200, "testkey")
-    assert stream == keystream(10, 200, "testkey")
+    stream = keystream(10, "testkey", 200)
+    assert stream == keystream(10, "testkey", 200)
     print("Same input, same output.")
-    assert stream != keystream(10, 200, "different key")
+    assert stream != keystream(10, "different key", 200)
     print("Different input, different output.")
-    assert len(keystream(42, 200, "testkey")) == 42
+    assert len(keystream(42, "testkey", 200)) == 42
     print("Requested 42 bytes of stream, got 42 bytes of stream.")
 
     print("-- Testing encryption. --")
     print("Encrypting 'fish', 200 rounds, key 'testkey', IV 'badiv'")
-    cipher = encrypt("fish", 200, "testkey", "badiv")
-    assert cipher == encrypt("fish", 200, "testkey", "badiv")
+    cipher = encrypt("fish", "testkey", 200, "badiv")
+    assert cipher == encrypt("fish", "testkey", 200, "badiv")
     print("Same input, same output.")
-    assert cipher != encrypt("fish", 200, "testkey")
+    assert cipher != encrypt("fish", "testkey", 200)
     print("Random IV, different output.")
     iv = random_iv()
-    random_cipher = encrypt("fish", 200, "testkey", iv)
+    random_cipher = encrypt("fish", "testkey", 200, iv)
     assert random_cipher.startswith(iv)
     print("Ciphertext starts with randomly-chosen IV.")
 
     print("-- Testing decryption. --")
-    assert "fish" == decrypt(cipher, 200, "testkey", 5)
+    assert "fish" == decrypt(cipher, "testkey", 200, 5)
     print("Got 'fish' back from stream with selected IV.")
-    assert "fish" == decrypt(random_cipher, 200, "testkey")
+    assert "fish" == decrypt(random_cipher, "testkey", 200)
     print("Got 'fish' back from stream with random IV.")
-    assert "fish" != decrypt(random_cipher, 200, "badkey")
+    assert "fish" != decrypt(random_cipher, "badkey", 200)
     print("Didn't get 'fish' back when using the wrong key.")
