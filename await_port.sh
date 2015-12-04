89a2008c323b42c05ef4d70075ce99ebe0bce298
Finn Ellis <relsqui@chiliahedron.com>
2015-12-03 19:25:06 -0800

    Add license info and description to await_port.sh.

--- await_port.sh
+++ await_port.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright (c) 2015 Finn Ellis, licensed under the MIT license.
+# (See accompanying LICENSE file for details.)
+
+# Continuously checks whether the TauNet port appears in netstat's
+# default output (i.e. is bound) and doesn't exit until it's not.
+# Suggested usage: ./await_port.sh; ./taurusd.py &
+
 echo -n 'Waiting for port ...'
 while netstat | grep -q 6283; do
     echo -n '.'
